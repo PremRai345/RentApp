8fe3f903a084f54d1d8f76239c6eff4201f61556
Prem Kumar Rai <69492512+PremRai345@users.noreply.github.com>
2022-03-07 22:24:44 +0545

    Bug fix
    Bug fix

--- rent_app/lib/screens/home_screen.dart
+++ rent_app/lib/screens/home_screen.dart
@@ -3,50 +3,57 @@ import 'package:rent_app/constants/constants.dart';
 import 'package:rent_app/screens/profile_screen.dart';
 import 'package:rent_app/utils/navigate.dart';
 import 'package:rent_app/utils/size_config.dart';
+import 'package:rent_app/widgets/curved_body_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  final image =
+      "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Welcome Home!"),
-        elevation: 0,
       ),
-      backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
       drawer: Drawer(
           child: Column(
         children: [
-          const UserAccountsDrawerHeader(
-            accountName: Text(
-              "Prem",
+          UserAccountsDrawerHeader(
+            accountName: const Text("Prem"),
+            accountEmail: const Text(
+              "prem@gmail.com",
             ),
-            accountEmail: Text(
-              " prem@gmail.com",
+            currentAccountPicture: Hero(
+              tag: "image-url",
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(image),
+              ),
             ),
+            // currentAccountPictureSize: Size.fromHeight(
+            //   SizeConfig.height * 5,
+            // ),
           ),
           ListTile(
-            title: Text("Profile"),
-            trailing: Icon(Icons.person),
-            onTap: navigate(context, ProfileScreen()),
-          ),
+            title: const Text("Profile"),
+            trailing: const Icon(
+              Icons.arrow_right_outlined,
+            ),
+            onTap: () => navigate(
+              context,
+              ProfileScreen(
+                imageUrl: image,
+              ),
+            ),
+          )
         ],
       )),
-      body: Container(
-        height: SizeConfig.height * 100,
-        width: SizeConfig.width * 100,
-        decoration: const BoxDecoration(
-          borderRadius: BorderRadius.vertical(top: Radius.circular(25)),
-          color: Colors.white,
-        ),
-        padding: basePadding,
-        child: SingleChildScrollView(
+      body: CurvedBodyWidget(
+        widget: SingleChildScrollView(
           child: Column(
             children: [
-              Text(
-                "Rent App",
-              ),
+              Text("Rent App"),
             ],
           ),
         ),
--- rent_app/lib/screens/profile_screen.dart
+++ rent_app/lib/screens/profile_screen.dart
@@ -1,14 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:rent_app/utils/size_config.dart';
+import 'package:rent_app/utils/validation_mixin.dart';
+import 'package:rent_app/widgets/curved_body_widget.dart';
+import 'package:rent_app/widgets/general_text_field.dart';
 
 class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({Key? key}) : super(key: key);
+  ProfileScreen({required this.imageUrl, Key? key}) : super(key: key);
+
+  final String imageUrl;
+  final nameController = TextEditingController();
+  final addressController = TextEditingController();
+  final ageController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profile'),
+        title: const Text("Profile"),
       ),
+      body: CurvedBodyWidget(
+          widget: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              Hero(
+                tag: "image-url",
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(imageUrl),
+                  radius: SizeConfig.height * 8,
+                ),
+              ),
+              SizedBox(
+                height: SizeConfig.height * 2,
+              ),
+              Text(
+                "Edit your profile",
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              SizedBox(
+                height: SizeConfig.height * 2,
+              ),
+              GeneralTextField(
+                title: "Name",
+                controller: nameController,
+                textInputType: TextInputType.name,
+                textInputAction: TextInputAction.next,
+                validate: (value) => ValidationMixin().validate(value!, "name"),
+                onFieldSubmitted: (_) {},
+              ),
+              SizedBox(
+                height: SizeConfig.height * 1.5,
+              ),
+              GeneralTextField(
+                title: "Address",
+                controller: addressController,
+                textInputType: TextInputType.streetAddress,
+                textInputAction: TextInputAction.next,
+                validate: (value) =>
+                    ValidationMixin().validate(value!, "address"),
+                onFieldSubmitted: (_) {},
+              ),
+              SizedBox(
+                height: SizeConfig.height * 1.5,
+              ),
+              GeneralTextField(
+                title: "Age",
+                controller: ageController,
+                maxLength: 3,
+                textInputType: TextInputType.number,
+                textInputAction: TextInputAction.done,
+                validate: (value) => ValidationMixin().validateAge(
+                  value!,
+                ),
+                onFieldSubmitted: (_) {},
+              ),
+              SizedBox(
+                height: SizeConfig.height * 2,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {}
+                },
+                child: Text("Save"),
+              ),
+            ],
+          ),
+        ),
+      )),
     );
   }
 }
